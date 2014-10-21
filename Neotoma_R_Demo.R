ccb607f6b5c9ea82c2c3f8ca852eef7b8a56da3a
IceAgeEcologist <jww@geography.wisc.edu>
2014-10-21 11:55:29 -0700

    Adding second half of demo with examples  for get_download

--- Neotoma_R_Demo.R
+++ Neotoma_R_Demo.R
@@ -2,6 +2,8 @@
 #Simon Goring & Jack Williams
 #Last Updated:  Oct 21, 2014
 #
+#This script is available at:  https://github.com/IceAgeEcologist/neotoma_demo
+#
 #See also:  Goring et al. (in review) Open Quaternary
 #neotoma: A Programmatic Interface to the Neotoma Paleoecological Database
 #https://github.com/SimonGoring/neotoma_paper
@@ -16,15 +18,50 @@ library(neotoma)
 #Get Site Information
 #get_site returns a data frame with information about 
 #Note use of % as wildcard operator
-marion <- get_site(sitename = 'Marion%')
-marion
+marion.meta.site <- get_site(sitename = 'Marion%')
+marion.meta.site
 
 #Get Dataset Information
-#get_dataset returns a list of datasets and the metadata for each dataset
+#get_dataset returns a list of datasets containing the metadata for each dataset
 #we can pass output from get_site to get_dataset
-marion.data  <- get_dataset(marion)
-#Let's look at the data returned for Marion Lake - in  this case the first of two sites.
-marion.data[[1]]
+marion.meta.data  <- get_dataset(marion.meta.site)
+#Let's look at the metadata returned for Marion Lake - in  this case the first of two sites.
+marion.meta.data[[1]]
 
 #Get Dataset
-#get_download 
+#get_download returns a list which stores a list download objects - one for each retrieved dataset.
+#Each download object contains a suite of data for the samples in that dataset
+marion.data <- get_download(marion.meta.site)
+
+#Within the download object, sample.meta stores the core depth and age information for that dataset
+#We just want to look at the first few lines, so are  using the head function
+head(marion.data[[1]]$sample.meta)
+
+#taxon.list stores a list of taxa found  in the  dataset
+head(marion.data[[1]]$taxon.list)
+
+#counts stores the the counts, presence/absence data, or percentage data for each taxon for each sample
+head(marion.data[[1]]$counts)
+
+#lab.data stores any associated  laboratory measurements in the dataset
+#(no data for marion...)
+head(marion.data[[1]]$lab.data)
+
+#Making a quick plot of Alnus abundances at Marion Lake, using the analogue package 
+#to calculate pollen percentages
+#load analogue
+install.packages("analogue")
+library("analogue")
+
+#Obtain all the pollen counts for Alnus from Marion Lake
+marion.alnus <- tran(x = marion.data[[1]]$counts, method = 'percent')[,'Alnus']
+
+#Build a dataframe containing the pollen counts and sample ages
+alnus.df <- data.frame(alnus = marion.alnus,
+                       ages  = marion.data[[1]]$sample.meta$age,
+                       site = rep('Marion', length(marion.alnus)))
+
+#Plot the  data
+plot(alnus ~ ages, data = alnus.df, col = alnus.df$site, pch = 19,
+     xlab = 'Radiocarbon Years Before Present', ylab = 'Percent Alnus')
+
